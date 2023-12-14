92620c6e697a865593ac100b9c663bfa57ed9653
siltonIT <140279319+siltonIT@users.noreply.github.com>
2023-12-14 12:02:07 +0300

    FIX

--- Task 6.2/Task 6.2.cpp
+++ Task 6.2/Task 6.2.cpp
@@ -61,8 +61,24 @@ void fill_random(Clocks* clock, const int SIZE)
 //Функция ищет дубликаты в массиве
 void find_duplicate(Clocks* clock, const int SIZE)
 {
+    Clocks dup;
+    dup.bitfield.hour = -1;
+    dup.bitfield.minute = -1;
+    dup.bitfield.second = -1;
+
     for (int i = 0; i < SIZE - 1; ++i)
     {
+        if (clock[i].bitfield.hour == dup.bitfield.hour)
+        {
+            if (clock[i].bitfield.minute == dup.bitfield.minute)
+            {
+                if (clock[i].bitfield.second == dup.bitfield.second)
+                {
+                    continue;
+                }
+            }
+        }
+
         if (clock[i].bitfield.hour == clock[i + 1].bitfield.hour)
         {
             if (clock[i].bitfield.minute == clock[i + 1].bitfield.minute)
@@ -70,6 +86,10 @@ void find_duplicate(Clocks* clock, const int SIZE)
                 if (clock[i].bitfield.second == clock[i + 1].bitfield.second)
                 {
                     print(clock[i]);
+                    dup.bitfield.hour = clock[i].bitfield.hour;
+                    dup.bitfield.minute = clock[i].bitfield.minute;
+                    dup.bitfield.second = clock[i].bitfield.second;
+
                 }
             }
         }
@@ -86,7 +106,7 @@ bool compare(Clocks left, Clocks right)
             return left.bitfield.second < right.bitfield.second;
         }
 
-        return left.bitfield.minute < left.bitfield.minute;
+        return left.bitfield.minute < right.bitfield.minute;
     }
 
     return left.bitfield.hour < right.bitfield.hour;
@@ -101,10 +121,44 @@ void print(Clocks clock)
 }
 
 /*
-clock[0].bitfield.hour = 1;
-clock[0].bitfield.minute = 1;
-clock[0].bitfield.second = 1;
-clock[1].bitfield.hour = 1;
-clock[1].bitfield.minute = 1;
-clock[1].bitfield.second = 1;
+
+    clock[0].bitfield.hour = 0;
+    clock[0].bitfield.minute = 0;
+    clock[0].bitfield.second = 0;
+
+    clock[1].bitfield.hour = 0;
+    clock[1].bitfield.minute = 0;
+    clock[1].bitfield.second = 1;
+
+    clock[2].bitfield.hour = 0;
+    clock[2].bitfield.minute = 2;
+    clock[2].bitfield.second = 0;
+
+    clock[3].bitfield.hour = 1;
+    clock[3].bitfield.minute = 2;
+    clock[3].bitfield.second = 1;
+
+    clock[4].bitfield.hour = 1;
+    clock[4].bitfield.minute = 0;
+    clock[4].bitfield.second = 1;
+
+    clock[5].bitfield.hour = 1;
+    clock[5].bitfield.minute = 1;
+    clock[5].bitfield.second = 1;
+
+    clock[6].bitfield.hour = 1;
+    clock[6].bitfield.minute = 1;
+    clock[6].bitfield.second = 1;
+
+    clock[7].bitfield.hour = 2;
+    clock[7].bitfield.minute = 2;
+    clock[7].bitfield.second = 2;
+
+    clock[8].bitfield.hour = 2;
+    clock[8].bitfield.minute = 2;
+    clock[8].bitfield.second = 2;
+
+    clock[9].bitfield.hour = 2;
+    clock[9].bitfield.minute = 2;
+    clock[9].bitfield.second = 2;
 */
